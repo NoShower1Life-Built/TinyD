5ea31a8ba24d13ffb8e51250e1a10416a67219bc
Donnie White <noshowerlife@gmail.com>
2026-08-30 23:11:02 -0500

    feat(execution): export production execution worker

--- execution/__init__.py
+++ execution/__init__.py
@@ -1,5 +1,6 @@
 from .ledger import ExecutionLedger
 from .runner import ExecutionRunner
 from .types import ExecutionRecord, ExecutionState
+from .worker import ExecutionWorker
 
-__all__ = ["ExecutionLedger", "ExecutionRecord", "ExecutionRunner", "ExecutionState"]
+__all__ = ["ExecutionLedger", "ExecutionRecord", "ExecutionRunner", "ExecutionState", "ExecutionWorker"]
