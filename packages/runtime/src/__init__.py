12e1f5c84986accf60f3aac659847cd39fdd37d8
Donnie White <noshowerlife@gmail.com>
2026-09-11 22:41:28 -0500

    feat: export legacy adapter contract

--- packages/runtime/src/__init__.py
+++ packages/runtime/src/__init__.py
@@ -1,6 +1,8 @@
 """TinyD deterministic runtime package."""
 
 from .engine import RuntimeEngine
+from .event_adapter import LegacyEventAdapter, LegacyEventAdapterError
+from .event_mapping import MAPPING_VERSION, EventMappingError, get_mapping
 from .journal import (
     EventIntegrityError,
     EventJournalError,
@@ -12,8 +14,13 @@ from .journal import (
 __all__ = [
     "EventIntegrityError",
     "EventJournalError",
+    "EventMappingError",
     "EventSequenceConflict",
     "EventTenantError",
+    "LegacyEventAdapter",
+    "LegacyEventAdapterError",
+    "MAPPING_VERSION",
     "PostgresEventJournal",
     "RuntimeEngine",
+    "get_mapping",
 ]
