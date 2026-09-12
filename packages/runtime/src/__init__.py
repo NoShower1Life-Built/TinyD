d4a722ccb0f0dffcf18548f2dd5a968f8a03d905
Donnie White <noshowerlife@gmail.com>
2026-09-11 23:44:12 -0500

    feat: export durable scheduler API

--- packages/runtime/src/__init__.py
+++ packages/runtime/src/__init__.py
@@ -2,14 +2,18 @@
 
 from .engine import RuntimeEngine
 from .event_journal import EventJournal, EventStorePort
+from .scheduler import DurableScheduler, SchedulableEvent, WorkItem
 from .worker import RuntimeScheduler, RuntimeWorker, ScheduledEvent, build_worker
 
 __all__ = [
+    "DurableScheduler",
     "EventJournal",
     "EventStorePort",
     "RuntimeEngine",
     "RuntimeScheduler",
     "RuntimeWorker",
+    "SchedulableEvent",
     "ScheduledEvent",
+    "WorkItem",
     "build_worker",
 ]
