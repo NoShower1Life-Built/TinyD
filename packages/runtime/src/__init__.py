dae46ee90ad9e406b55a124b9b2e12c1b46bfeb3
Donnie White <noshowerlife@gmail.com>
2026-09-11 23:32:53 -0500

    feat(runtime): export scheduler worker boundary

--- packages/runtime/src/__init__.py
+++ packages/runtime/src/__init__.py
@@ -2,5 +2,14 @@
 
 from .engine import RuntimeEngine
 from .event_journal import EventJournal, EventStorePort
+from .worker import RuntimeScheduler, RuntimeWorker, ScheduledEvent, build_worker
 
-__all__ = ["EventJournal", "EventStorePort", "RuntimeEngine"]
+__all__ = [
+    "EventJournal",
+    "EventStorePort",
+    "RuntimeEngine",
+    "RuntimeScheduler",
+    "RuntimeWorker",
+    "ScheduledEvent",
+    "build_worker",
+]
