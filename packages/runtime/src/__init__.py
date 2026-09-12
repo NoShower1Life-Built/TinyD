4d9259b75d7c06da618d8085ffae0bef4596e226
Donnie White <noshowerlife@gmail.com>
2026-09-12 00:14:57 -0500

    feat(runtime): export failure recording outcome

--- packages/runtime/src/__init__.py
+++ packages/runtime/src/__init__.py
@@ -3,13 +3,14 @@
 from .engine import RuntimeEngine
 from .event_journal import EventJournal, EventStorePort
 from .scheduler import DurableScheduler, SchedulableEvent, WorkItem
-from .worker import EventResolver, RuntimeWorker, WorkerResult, build_worker
+from .worker import EventResolver, FailureRecordOutcome, RuntimeWorker, WorkerResult, build_worker
 
 __all__ = [
     "DurableScheduler",
     "EventJournal",
     "EventResolver",
     "EventStorePort",
+    "FailureRecordOutcome",
     "RuntimeEngine",
     "RuntimeWorker",
     "SchedulableEvent",
